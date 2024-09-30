2f3feea0d485a21c115ce7c63fa34ed9a635cf53
Harsh Jaiswal <harsh.jaiswal.raipur@gmail.com>
2024-09-30 18:44:43 +1000

    initial

---
+++ app/src/App.tsx
@@ -0,0 +1,68 @@
+
+import './App.css'
+import { useState } from 'react';
+
+function App() {
+  const [firstName, setFirstName] = useState("");
+  const [lasttName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmpassword, setConfirmPassword] = useState("");
+  
+
+ 
+  const handleClick = () => {
+    if (firstName === '') {
+      alert("No name provided");
+    } 
+    if (lasttName === '') {
+      alert("No LastName provided");
+    } 
+    if (email === '') {
+      alert("No Email provided");
+    } 
+    if (password === '') {
+      alert("No password provided");
+    } 
+    if (password != confirmpassword) {
+      alert("No password not equal to confirmpass");
+    } 
+    
+    
+  };
+  return (
+    <>
+    <h1>Registration Form </h1>
+    <form>
+      
+      <label>First Name</label>
+      <br></br>
+      <input value = {firstName} onChange = {(e) => setFirstName(e.target.value)}/>
+      <br></br>
+      <br></br>
+      <label>Last Name</label>
+      <br></br>
+      <input value = {lasttName} onChange = {(e) => setLastName(e.target.value)}/>      <br></br>
+      <br></br>
+      <label>Email</label>
+      <br></br>
+      <input value = {email} onChange = {(e) => setEmail(e.target.value)}/>
+      <br></br>
+      <br></br>
+      <label>Password</label>
+      <br></br>
+      <input value = {password} onChange = {(e) => setPassword(e.target.value)}/>
+      <br></br>
+      <br></br>
+      <label> Confirm Password</label>
+      <br></br>
+      <input value = {confirmpassword} onChange = {(e) => setConfirmPassword(e.target.value)}/>
+    </form>
+    <br></br>
+    <button onClick={handleClick}>Submit</button>
+
+    </>
+  )
+}
+
+export default App
